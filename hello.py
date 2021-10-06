925bee6ab4dd852e54cf8215e7f6ef1f7f735ed1
Luka Krajina <luka.krajina@bulme.at>
2021-10-06 08:54:49 +0200

    Added datetime + date related message

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import datetime
+currentTime = datetime.datetime.now()
 name = input("Hallo. Wie heißen sie?\n")
 #print("Hallo ", name, "!")
 #Alternativ:
-print(f'Guten Tag, {name}!')
+print(f'Guten Tag, {name}! Es ist der {currentTime}.')
